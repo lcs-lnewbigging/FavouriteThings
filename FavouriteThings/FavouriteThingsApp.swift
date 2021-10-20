1edbce17c560663859161d44577b3811c81929c7
Luke Newbigging <lnewbigging@lcs.on.ca>
2021-10-20 10:37:22 -0400

    Initial Commit

---
+++ FavouriteThings/FavouriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteThingsApp.swift
+//  FavouriteThings
+//
+//  Created by Luke Newbigging on 2021-10-20.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
