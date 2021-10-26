45e3168d42ce4ad06ab4782d8b8cbd55a001719d
Luke Newbigging <lnewbigging@lcs.on.ca>
2021-10-25 20:33:00 -0400

    I have all the navigational links and titles set up on the landing page, and all of the scrolling and filler text in each of the four "FavouriteThingsViews"

--- FavouriteThings/Views/FavouriteThingsFive.swift
+++ FavouriteThings/Views/FavouriteThingsFive.swift
@@ -15,7 +15,9 @@ struct FavouriteThingsFive: View {
                 Image("Bahamas")
                     .resizable()
                     .scaledToFit()
-                Text("This is a beach on the souther tip of Cat Island in the bahamas. I went here for Christmas in 2019 with my family, it was an amazing experiance.")
+                Text("This is a beach on the souther tip of Cat Island in the bahamas. I went here for Christmas in 2019 with my family, it was an amazing experiance.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.")
+                    
+
             
                     .padding()
                 
